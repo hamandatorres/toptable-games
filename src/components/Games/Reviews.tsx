169b3143a4b77275ef669733382a81b3c1ac658e
Carlos A Chavez Perez <hamandatorres@gmail.com>
2025-08-23 12:09:56 -0600

    refactor: standardize code formatting and improve readability in multiple components

--- src/components/Games/Reviews.tsx
+++ src/components/Games/Reviews.tsx
@@ -1,47 +1,47 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
-import { Review, ReviewProps } from 'customTypes';
-import Rating from '../StyledComponents/Rating';
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+import { Review, ReviewProps } from "customTypes";
+import Rating from "../StyledComponents/Rating";
 
 const Reviews: React.FC<ReviewProps> = (props: ReviewProps) => {
-  const [review, setReview] = useState([]);
-  const game_id = props.game_id;
+	const [review, setReview] = useState([]);
+	const game_id = props.game_id;
 
-  const getGameReview = useCallback((): void => {
-    axios
-      .get(`/api/game/reviews/${game_id}`)
-      .then((res) => {
-        const reviewsArray = res.data;
-        setReview(reviewsArray);
-      })
-      .catch((err) => console.log(err));
-  }, [game_id]);
+	const getGameReview = useCallback((): void => {
+		axios
+			.get(`/api/game/reviews/${game_id}`)
+			.then((res) => {
+				const reviewsArray = res.data;
+				setReview(reviewsArray);
+			})
+			.catch((err) => console.log(err));
+	}, [game_id]);
 
-  useEffect((): void => {
-    getGameReview();
-  }, [getGameReview]);
+	useEffect((): void => {
+		getGameReview();
+	}, [getGameReview]);
 
-  const mappedReviews = review.map((elem: Review, id: number) => {
-    if (elem.rating || elem.review) {
-      return (
-        <article className="reviewContainer" key={id}>
-          <Rating rating={elem.rating} />
-          <h5 className="usernameHeader">{elem.username}</h5>
-          <br />
-          <div className="reviewBox">{elem.review}</div>
-        </article>
-      );
-    } else {
-      return null;
-    }
-  });
+	const mappedReviews = review.map((elem: Review, id: number) => {
+		if (elem.rating || elem.review) {
+			return (
+				<article className="reviewContainer" key={id}>
+					<Rating rating={elem.rating} />
+					<h5 className="usernameHeader">{elem.username}</h5>
+					<br />
+					<div className="reviewBox">{elem.review}</div>
+				</article>
+			);
+		} else {
+			return null;
+		}
+	});
 
-  return (
-    <>
-      {mappedReviews[0] ? <h4>Reviews:</h4> : ''}
-      {mappedReviews}
-    </>
-  );
+	return (
+		<>
+			{mappedReviews[0] ? <h4>Reviews:</h4> : ""}
+			{mappedReviews}
+		</>
+	);
 };
 
 export default Reviews;
--- src/components/User/PlayCountGraph.tsx
+++ src/components/User/PlayCountGraph.tsx
@@ -1,80 +1,93 @@
-import React, { useEffect, useState, useRef, useMemo } from 'react';
-import { Chart, ChartConfiguration, registerables } from 'chart.js';
-import { useSelector } from 'react-redux';
-import { UserGame } from '../../redux/userGameReducer';
-import { RootState } from '../../redux/store';
+import React, { useEffect, useState, useRef, useMemo } from "react";
+import { Chart, ChartConfiguration, registerables } from "chart.js";
+import { useSelector } from "react-redux";
+import { UserGame } from "../../redux/userGameReducer";
+import { RootState } from "../../redux/store";
 
 const PlayCountGraph: React.FC = () => {
-  const [userGamesLabels, setUserGamesLabels] = useState<string[]>([]);
-  const [userGamesPlays, setUserGamesPlays] = useState<number[]>([]);
-  const [chartInstance, setChartInstance] = useState<Chart<'bar'>>();
+	const [userGamesLabels, setUserGamesLabels] = useState<string[]>([]);
+	const [userGamesPlays, setUserGamesPlays] = useState<number[]>([]);
+	const [chartInstance, setChartInstance] = useState<Chart<"bar">>();
 
-  const chartContainer = useRef<HTMLCanvasElement | null>(null);
-  const userGames: UserGame[] = useSelector((state: RootState) => state.userGameReducer.userGames);
+	const chartContainer = useRef<HTMLCanvasElement | null>(null);
+	const userGames: UserGame[] = useSelector(
+		(state: RootState) => state.userGameReducer.userGames
+	);
 
-  useEffect(() => {
-    setUserGamesLabels(userGamesSorter(userGames).userGamesLabels);
-    setUserGamesPlays(userGamesSorter(userGames).userGamesPlays);
-  }, [userGames]);
+	useEffect(() => {
+		setUserGamesLabels(userGamesSorter(userGames).userGamesLabels);
+		setUserGamesPlays(userGamesSorter(userGames).userGamesPlays);
+	}, [userGames]);
 
-  const userGamesSorter = (userGames: UserGame[]): { userGamesLabels: string[]; userGamesPlays: number[] } => {
-    const userGamesSorted: UserGame[] = userGames;
+	const userGamesSorter = (
+		userGames: UserGame[]
+	): { userGamesLabels: string[]; userGamesPlays: number[] } => {
+		const userGamesSorted: UserGame[] = userGames;
 
-    userGamesSorted.sort((firstEl: UserGame, secondEl: UserGame) => {
-      if (firstEl.play_count < secondEl.play_count) {
-        return 1;
-      }
-      if (firstEl.play_count > secondEl.play_count) {
-        return -1;
-      }
-      // a must be equal to b
-      return 0;
-    });
+		userGamesSorted.sort((firstEl: UserGame, secondEl: UserGame) => {
+			if (firstEl.play_count < secondEl.play_count) {
+				return 1;
+			}
+			if (firstEl.play_count > secondEl.play_count) {
+				return -1;
+			}
+			// a must be equal to b
+			return 0;
+		});
 
-    const outputMain = userGamesSorted.slice(0, 5);
-    const userGamesPlays = outputMain.map((el: UserGame) => {
-      return el.play_count ? el.play_count : 0;
-    });
-    const userGamesLabels = outputMain.map((el: UserGame) => el.name);
+		const outputMain = userGamesSorted.slice(0, 5);
+		const userGamesPlays = outputMain.map((el: UserGame) => {
+			return el.play_count ? el.play_count : 0;
+		});
+		const userGamesLabels = outputMain.map((el: UserGame) => el.name);
 
-    return {
-      userGamesPlays,
-      userGamesLabels
-    };
-  };
+		return {
+			userGamesPlays,
+			userGamesLabels,
+		};
+	};
 
-  const chartConfig: ChartConfiguration<'bar'> = useMemo(() => ({
-    type: 'bar',
-    data: {
-      labels: userGamesLabels,
-      datasets: [
-        {
-          label: 'Most Played Games',
-          data: userGamesPlays
-        }
-      ]
-    },
-    options: {
-      plugins: {
-        legend: {}
-      }
-    }
-  }), [userGamesLabels, userGamesPlays]);
+	const chartConfig: ChartConfiguration<"bar"> = useMemo(
+		() => ({
+			type: "bar",
+			data: {
+				labels: userGamesLabels,
+				datasets: [
+					{
+						label: "Most Played Games",
+						data: userGamesPlays,
+					},
+				],
+			},
+			options: {
+				plugins: {
+					legend: {},
+				},
+			},
+		}),
+		[userGamesLabels, userGamesPlays]
+	);
 
-  useEffect(() => {
-    if (chartContainer && chartContainer.current) {
-      chartInstance?.destroy();
-      Chart.register(...registerables);
-      const newChart = new Chart(chartContainer.current, chartConfig);
-      setChartInstance(newChart);
-    }
-  }, [chartContainer, userGamesLabels, userGamesPlays, chartConfig, chartInstance]);
+	useEffect(() => {
+		if (chartContainer && chartContainer.current) {
+			chartInstance?.destroy();
+			Chart.register(...registerables);
+			const newChart = new Chart(chartContainer.current, chartConfig);
+			setChartInstance(newChart);
+		}
+	}, [
+		chartContainer,
+		userGamesLabels,
+		userGamesPlays,
+		chartConfig,
+		chartInstance,
+	]);
 
-  return (
-    <div>
-      <canvas ref={chartContainer} />
-    </div>
-  );
+	return (
+		<div>
+			<canvas ref={chartContainer} />
+		</div>
+	);
 };
 
 export default PlayCountGraph;
