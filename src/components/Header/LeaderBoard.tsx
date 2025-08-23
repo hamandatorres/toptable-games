8317813cc75c54df831594c11c45e9fd7b920019
Carlos A Chavez Perez <hamandatorres@gmail.com>
2025-08-23 12:31:41 -0600

    Fix build errors: Replace missing medal images with CSS elements and update background to gradient

--- src/components/Header/LeaderBoard.tsx
+++ src/components/Header/LeaderBoard.tsx
@@ -2,12 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Player } from 'customTypes';
 
-import Gold from '../../Images/TTGMedalGold.png';
-import Silver from '../../Images/TTGMedalSilver.png';
-import Bronze from '../../Images/TTGMedalBronze.png';
-import Black from '../../Images/TTGMedalBlack.png';
-
-const LeaderBoard: React.FC = (): JSX.Element => {
+const LeaderBoard: React.FC = () => {
   const [leaders, setLeaders] = useState<Player[]>([]);
   const [firstUsername, setFirstUsername] = useState('');
   const [firstTotal, setFirstTotal] = useState('');
@@ -61,35 +56,35 @@ const LeaderBoard: React.FC = (): JSX.Element => {
       <div className="users">
         <div className="leaderRow">
           <div className="position">
-            <img className="medals" src={Gold} alt='Gold Medal' />
+            <div className="medal medal-gold">🥇</div>
             <p>{firstUsername}</p>
           </div>
           <p>{firstTotal}</p>
         </div>
         <div className="leaderRow">
           <div className="position">
-            <img className="medals" src={Silver} alt='Silver Medal' />
+            <div className="medal medal-silver">🥈</div>
             <p>{secondUsername}</p>
           </div>
           <p>{secondTotal}</p>
         </div>
         <div className="leaderRow">
           <div className="position">
-            <img className="medals" src={Bronze} alt='Bronze Medal' />
+            <div className="medal medal-bronze">🥉</div>
             <p>{thirdUsername}</p>
           </div>
           <p>{thirdTotal}</p>
         </div>
         <div className="leaderRow">
           <div className="position">
-            <img className="medals" src={Black} alt='Fourth Place' />
+            <div className="medal medal-fourth">4</div>
             <p>{fourthUsername}</p>
           </div>
           <p>{fourthTotal}</p>
         </div>
         <div className="leaderRow">
           <div className="position">
-            <img className="medals" src={Black} alt='Fifth Place' />
+            <div className="medal medal-fifth">5</div>
             <p>{fifthUsername}</p>
           </div>
           <p>{fifthTotal}</p>
